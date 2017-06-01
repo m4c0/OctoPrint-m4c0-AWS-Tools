e18b9ca4048ea4be6569e9654a596f6242b8a615
Eduardo Costa <ecosta@tpk.com.br>
2017-06-01 23:41:41 +0100

    basic metrics publishing

--- octoprint_m4c0_aws_tools/temperature_probe.py
+++ octoprint_m4c0_aws_tools/temperature_probe.py
@@ -1,10 +1,28 @@
 # coding=utf-8
 
+from datetime import datetime
+
+import boto3
 import octoprint.printer
 
 class temperature_probe(octoprint.printer.PrinterCallback):
 
+	def __init__(self):
+		self._cloudwatch = boto3.client('cloudwatch')
+
 	def on_printer_add_temperature(self, data):
-		# Prints {'tool0': {'actual': 22.5, 'target': 0.0}, 'bed': {'actual': 22.9, 'target': 0.0}, 'time': 1496353649}
-		print data
+		timestamp = datetime.fromtimestamp(data['time'])
+
+		metrics = [
+			self._build_metric('tool0', data['tool0'], timestamp),
+			self._build_metric('bed', data['bed'], timestamp)
+			]
+
+		self._cloudwatch.put_metric_data(Namespace='OctoPrint/Temperature', MetricData=metrics)
+
+	def _build_metric(self, tool, data, timestamp):
+		metric = dict(Unit='None', Timestamp=timestamp)
+		metric['MetricName'] = tool
+		metric['Value'] = data['actual']
+		return metric
 
